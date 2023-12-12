5393abe5118415f1a3ad1d00449e77e9eed14ac6
leticiakaorism <146925752+leticiakaorism@users.noreply.github.com>
2023-12-12 16:35:43 +1100

    Added basic JS and added typewriter

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function generateRecipe(event) {
+    event.preventDefault();
+
+    new Typewriter('#recipe', {
+        strings: 'Are you ready to make delicious food? 😋',
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+    
+}
+
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
